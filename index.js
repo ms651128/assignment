d7349b0ca495b6fbe2b7d414d13f2f47bb5e29b9
Rahul Sharma <ms651128@gmail.com>
2023-07-31 14:50:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const data = require('./data.json');
+const bodyParser = require('body-parser');
+server = express();
+
+
+server.use(express.static('views'));
+server.set('view engine', 'ejs');
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.get('/',(req,res)=>{
+    res.render('index',{data:data});
+})
+
+server.post('/flight', async(req,res)=>{
+    const {source,destination,date} = req.body; 
+    const flights = data.flights.filter(flight => flight.source.toLowerCase() === source.toLowerCase() && flight.destination.toLowerCase() === destination.toLowerCase());
+    
+    if (date) {
+        flights = flights.filter((flight) => flight.date === date);
+    }
+    
+    if(flights.length===0)
+    {
+        res.render('index',{data:{flights:flights}});
+    }
+    else{
+        res.render('index',{data:{flights:flights}});
+    }   
+})
+
+const port = process.env.port || 8080;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+  
